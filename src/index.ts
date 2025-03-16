22ef63d967c10420c48180b59cf65a8fca108eae
Patrick Bucher <patrick.bucher@mailbox.org>
2025-03-16 14:16:34 +0100

    find pythagoraen triplets

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const lang: string = "TypeScript";
-const greeting: string = `Hello, ${lang}!`;
-console.log(greeting);
+import { isTriplet } from "./pythagoras";
+
+const MAX: number = 100;
+for (let a = 1; a < MAX; a++) {
+  for (let b = 1; b < MAX; b++) {
+    for (let c = 1; c < MAX; c++) {
+      if (isTriplet(a, b, c)) {
+        console.log(`${a}²+${b}²=${c}²`);
+      }
+    }
+  }
+}
